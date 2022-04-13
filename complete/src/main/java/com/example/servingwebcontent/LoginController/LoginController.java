a505a07e0bc1d17a96299eb5ec9601059edeeeb1
POON_YEONG_SHIAN <yeon0001@student.monash.edu>
2022-04-13 13:56:33 +0800

    link login with login/user api

--- complete/src/main/java/com/example/servingwebcontent/LoginController/LoginController.java
+++ complete/src/main/java/com/example/servingwebcontent/LoginController/LoginController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @Controller
 public class LoginController {
     @GetMapping("/")
@@ -24,10 +30,10 @@ public class LoginController {
 
     //Check for Credentials
     @PostMapping("/login")
-    public String postlogin(@ModelAttribute(name="userlogin") UserLogin login, Model m) {
+    public String postlogin(@ModelAttribute(name="userlogin") UserLogin login, Model m) throws IOException, InterruptedException {
         String uname = login.getUserName();
         String pass = login.getPassword();
-        if(uname.equals("Admin") && pass.equals("Admin")) {
+        if(checkLoginFromAPI(uname, pass)) {
             m.addAttribute("userName", uname);
             m.addAttribute("password", pass);
             return "testing-site";
@@ -42,5 +48,32 @@ public class LoginController {
     public String postlogin(@ModelAttribute UserLogin userLogin, BindingResult result, Model model) {
         model.addAttribute("UserLogin", userLogin);
         return "testing-site";
+    }
+
+
+    public boolean checkLoginFromAPI(String userName, String password) throws IOException, InterruptedException {
+        boolean flag = false;
+        String myApiKey = "NrMhfCkHTjJjzHTWR8z8nP6FjcGg8K";
+        String rootUrl = "https://fit3077.com/api/v1";
+        String usersUrl = rootUrl + "/user";
+        String usersLoginUrl = usersUrl + "/login";
+
+        String jsonString = "{" +
+                "\"userName\":\"" + userName + "\"," +
+                "\"password\":\"" + password + "\"" +
+                "}";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(usersLoginUrl + "?jwt=true")) // Return a JWT so we can use it in Part 5 later.
+                .setHeader("Authorization", myApiKey)
+                .header("Content-Type","application/json") // This header needs to be set when sending a JSON request body.
+                .POST(HttpRequest.BodyPublishers.ofString(jsonString))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if(response.statusCode() == 200){
+            flag = true;
+        }
+        return flag;
     }
 }
