d82d3a0f849b12715f5e807d42c1a49719c56a2d
POON_YEONG_SHIAN <yeon0001@student.monash.edu>
2022-04-13 10:42:03 +0800

    complete simple login function

--- complete/src/main/java/com/example/servingwebcontent/LoginController/LoginController.java
+++ complete/src/main/java/com/example/servingwebcontent/LoginController/LoginController.java
@@ -10,11 +10,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("UserLogin", new UserLogin());
+        return "index";
+    }
+
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("UserLogin", new UserLogin());
         return "login";
     }
+
+    //Check for Credentials
+    @PostMapping("/login")
+    public String postlogin(@ModelAttribute(name="userlogin") UserLogin login, Model m) {
+        String uname = login.getUserName();
+        String pass = login.getPassword();
+        if(uname.equals("Admin") && pass.equals("Admin")) {
+            m.addAttribute("uname", uname);
+            m.addAttribute("pass", pass);
+            return "testing-site";
+        }
+        m.addAttribute("error", "Incorrect Username & Password");
+        return "/login";
+
+    }
+
 
     @PostMapping("/testing-site")
     public String postlogin(@ModelAttribute UserLogin userLogin, BindingResult result, Model model) {
