e7aaa35d1e23e73ada9d39738beab22934824ec4
POON_YEONG_SHIAN <yeon0001@student.monash.edu>
2022-04-17 19:16:43 +0800

    remove model package, modified the OnSiteRegisterController

--- complete/src/main/java/com/example/servingwebcontent/controllers/OnSiteRegisterController.java
+++ complete/src/main/java/com/example/servingwebcontent/controllers/OnSiteRegisterController.java
@@ -5,15 +5,11 @@ import com.example.servingwebcontent.api.Get;
 import com.example.servingwebcontent.api.TestingSiteFactory;
 import com.example.servingwebcontent.apiclasses.TestType;
 import com.example.servingwebcontent.apiclasses.TestingSite;
-import com.example.servingwebcontent.domain.UserLogin;
-import com.example.servingwebcontent.model.SMSPinModel;
-import com.example.servingwebcontent.model.Test;
-import com.example.servingwebcontent.model.TestTypeModel;
-import com.example.servingwebcontent.model.TestingSiteModel;
+import com.example.servingwebcontent.domain.BookingForm;
+
 import com.example.servingwebcontent.tool.RandomPinGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,27 +25,24 @@ public class OnSiteRegisterController {
 
     @GetMapping("/register")
     public String getRegister(Model model) {
-        model.addAttribute("UserLogin", new UserLogin());
+        //1.
+        BookingForm bookingForm = new BookingForm();
+        model.addAttribute("bookingForm", bookingForm);
 
-        // Demo purpose
-        List<Test> tests = new ArrayList<>();
-        tests.add(new Test("heyman its mario", "why r u here", 3400));
-        tests.add(new Test("heyman its mom", "why r u here", 5700));
-        model.addAttribute("tests", tests);
 
-        // Get testing-sites and put it into model
+        //2. Get testing-sites and put it into model
         // API factory
         APIfactory factory = new TestingSiteFactory(api);
         Get testingSiteGet = factory.createGet();
 
-        List<TestingSiteModel> testingSiteModels = new ArrayList<>();
+        List<String> testingSiteModels = new ArrayList<>();
 
         try {
             // Testing-site collection
             Collection<TestingSite> testingSites = testingSiteGet.getApi();
             Iterator<TestingSite> iterator = testingSites.iterator();
             while (iterator.hasNext()) {
-                testingSiteModels.add(new TestingSiteModel(iterator.next().getName()));
+                testingSiteModels.add(iterator.next().getName());
             }
         }
         catch(Exception e) {
@@ -57,24 +50,23 @@ public class OnSiteRegisterController {
         }
         model.addAttribute("testingSiteModels", testingSiteModels);
 
-        // Get test-type and put it into model
-        List<TestTypeModel> testTypeModels = new ArrayList<>();
+        //3. Get test-type and put it into model
+        List<String> testTypeModels = new ArrayList<>();
         for (TestType testType : TestType.values()) {
-            testTypeModels.add(new TestTypeModel(testType+""));
+            testTypeModels.add(testType+"");
         }
         model.addAttribute("testTypeModels", testTypeModels);
 
-        // Get smsPin
+        //4. Get smsPin
         RandomPinGenerator rad = new RandomPinGenerator();
         String smsPin = rad.getPin();
-        SMSPinModel smsPinModel = new SMSPinModel(smsPin);
-//        model.addAttribute("smsPinModel", smsPinModel);
+        // model.addAttribute("smsPinModel", smsPinModel);
 
         return "register";
     }
     @PostMapping("/register")
-    public String postTestingSite(@ModelAttribute(name = "userlogin") UserLogin login, Model model) {
-        System.out.println("mom");
+    public String submitForm(@ModelAttribute("bookingForm") BookingForm bookingForm) {
+        System.out.println(bookingForm);
         return "register";
     }
 }
---
+++ complete/src/main/java/com/example/servingwebcontent/domain/BookingForm.java
@@ -0,0 +1,60 @@
+package com.example.servingwebcontent.domain;
+
+public class BookingForm {
+    private String customerUsername;
+    private String testingSite;
+    private String administrator;
+    private String patient;
+    private String testType;
+
+    public String getCustomerUsername() {
+        return customerUsername;
+    }
+
+    public void setCustomerUsername(String customerUsername) {
+        this.customerUsername = customerUsername;
+    }
+
+    public String getTestingSite() {
+        return testingSite;
+    }
+
+    public void setTestingSite(String testingSite) {
+        this.testingSite = testingSite;
+    }
+
+    public String getAdministrator() {
+        return administrator;
+    }
+
+    public void setAdministrator(String administrator) {
+        this.administrator = administrator;
+    }
+
+    public String getPatient() {
+        return patient;
+    }
+
+    public void setPatient(String patient) {
+        this.patient = patient;
+    }
+
+    public String getTestType() {
+        return testType;
+    }
+
+    public void setTestType(String testType) {
+        this.testType = testType;
+    }
+
+    @Override
+    public String toString() {
+        return "BookingForm{" +
+                "customerUsername='" + customerUsername + '\'' +
+                ", testingSite='" + testingSite + '\'' +
+                ", administrator='" + administrator + '\'' +
+                ", patient='" + patient + '\'' +
+                ", testType='" + testType + '\'' +
+                '}';
+    }
+}
