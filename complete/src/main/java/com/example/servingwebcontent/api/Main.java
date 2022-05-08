bc0ae2cf6e770bdf9cdbd2fc3fdac06edfdf39c1
POON_YEONG_SHIAN <yeon0001@student.monash.edu>
2022-05-08 15:00:27 +0800

    add TODAY

--- complete/src/main/java/com/example/servingwebcontent/api/Main.java
+++ complete/src/main/java/com/example/servingwebcontent/api/Main.java
@@ -19,5 +19,7 @@ public class Main {
         APIfactory testFactory = new BookingFactory(api);
         Delete bookingDelete = testFactory.createDelete();
         bookingDelete.deleteApi(covidTestId);
+
+        //TODAY
     }
 }
